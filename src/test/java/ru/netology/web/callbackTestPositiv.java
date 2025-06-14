5f58f05d6356301e32b1f1b9cec86b34715b45a5
MIVAKI <elenaananenko2605@gmail.com>
2025-06-15 00:27:36 +0300

    Positiv test

--- src/test/java/ru/netology/web/callbackTestPositiv.java
+++ src/test/java/ru/netology/web/callbackTestPositiv.java
@@ -50,7 +50,7 @@ class callbackTestPositiv {
         WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
         String actualText = actualElement.getText().trim();
 
-        assertEquals("Ваша заявка успешно отправлена!", actualText);
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualElement.isDisplayed());
     }
 }
